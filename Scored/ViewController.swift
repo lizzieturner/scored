57797ed1b82fcc60aba0ee3c85d8e212d5864c94
Lizzie Turner <hello@lizzieturner.com>
2018-09-25 14:13:36 -0700

    Initial Commit

---
+++ Scored/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scored
+//
+//  Created by Lizzie Turner on 9/25/18.
+//  Copyright © 2018 lizzieturner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
